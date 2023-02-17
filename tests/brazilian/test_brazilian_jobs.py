ffebcfa95d568bd75bc402771d54376673bb48c5
BrenoOPrado <breno.o.prado@hotmail.com>
2023-02-17 13:55:21 -0300

    Testes desenvolvidos para as finções count_ocurrences e read_brazilian_file :turtle:

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    file = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for item in file:
+        assert "title" in item
+        assert "salary" in item
+        assert "type" in item
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    counter_lower_case = count_ocurrences('data/jobs.csv', 'python')
+    counter_upper_case = count_ocurrences('data/jobs.csv', 'PYTHON')
+    counter_all_cases = count_ocurrences('data/jobs.csv', 'PyThOn')
+    assert counter_lower_case == 1639
+    assert counter_upper_case == 1639
+    assert counter_all_cases == 1639
